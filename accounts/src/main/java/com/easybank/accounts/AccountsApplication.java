24dd6f529efcbcd29724a5841862cea155980c13
songminkyu <rabbircarrot@naver.com>
2025-06-18 15:35:28 +0900

    Add OpenAPI documentation for Accounts microservice
    Introduced OpenAPIDefinition annotation to provide API metadata, including contact details, license, and external documentation link.

--- accounts/src/main/java/com/easybank/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/easybank/accounts/AccountsApplication.java
@@ -1,11 +1,39 @@
 package com.easybank.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.easybank.accounts.controller") })
+@EnableJpaRepositories("com.easybank.accounts.repository")
+@EntityScan("com.easybank.accounts.model")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "songminkyu",
+						email = "rabbircarrot@naver.com",
+						url = "https://github.com/songminkyu"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/songminkyu"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank Accounts microservice REST API Documentation",
+				url = "https://www.eazybank.com/swagger-ui.html"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
